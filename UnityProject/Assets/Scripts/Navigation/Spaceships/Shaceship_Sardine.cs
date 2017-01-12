befecf76f48326468672aa4a8854c95fdda616ea
DESKTOP-J5CUFKV\Thibaut <joethesardine@gmail.com>
2017-01-12 18:29:43 +0100

    Working on Sardine Spaceship

---
+++ UnityProject/Assets/Scripts/Navigation/Spaceships/Shaceship_Sardine.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shaceship_Sardine : SpaceshipController
+{
+	public GameObject _particle1;
+	public GameObject _particle2;
+}
---
+++ UnityProject/Assets/Scripts/Navigation/Spaceships/SpaceshipController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipController : MonoBehaviour
+{
+	new Rigidbody rigidbody;
+
+	public float rotationSpeed;
+	public float acceleration;
+
+	public GameObject _particle1;
+	public GameObject _particle2;
+	
+	void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Z))
+		{ // Accélérer
+			rigidbody.AddForce(transform.forward * acceleration);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{ // Démarrer les moteurs
+			rigidbody.useGravity = false;
+			_particle1.SetActive(true);
+			_particle2.SetActive(true);
+		}
+
+		if (Input.GetKey(KeyCode.L))
+		{ // Redresser
+			rigidbody.AddTorque(-transform.right * rotationSpeed);
+		}
+		if (Input.GetKey(KeyCode.E))
+		{ // Couper les moteurs
+			rigidbody.useGravity = true;
+			_particle1.SetActive(false);
+			_particle2.SetActive(false);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{ // Freiner
+			rigidbody.AddForce(-transform.forward * acceleration);
+		}
+		if (Input.GetKey(KeyCode.K))
+		{ // Pencher à gauche 
+			rigidbody.AddTorque(transform.forward * rotationSpeed);
+		}
+		if (Input.GetKey(KeyCode.O))
+		{ // Plonger
+			rigidbody.AddTorque(transform.right * rotationSpeed);
+		}
+		if (Input.GetKey(KeyCode.M))
+		{ // Pencher à droite
+			rigidbody.AddTorque(-transform.forward * rotationSpeed);
+		}
+	}
+}
