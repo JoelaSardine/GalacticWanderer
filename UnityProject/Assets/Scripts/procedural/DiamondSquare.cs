8fb79a3069619a3e7c2fa5098cf241ccc72c0b0a
Wh1t3L0tus <wh1t3l07u5@gmail.com>
2017-01-25 01:38:09 +0100

    feat(procedural): add vertex and normals generation

--- UnityProject/Assets/Scripts/procedural/DiamondSquare.cs
+++ UnityProject/Assets/Scripts/procedural/DiamondSquare.cs
@@ -3,31 +3,48 @@ using UnityEngine;
 
 public class DiamondSquare : MonoBehaviour {
 
-    public int step;
+    // Number of iteration to perform to generate the landscape
     public int iterationCount = 5;
+
+    // Height bounds of the landscape
     public Interval heightInterval = new Interval(-100, 100);
 
-    public GameObject prefab;
+    private Mesh mesh;
 
-    private float[] heights;
+    private int step;
+
+    // Generated vertices
+    private Vector3[] vertices;
+
+    // Generated normals
+    private Vector3[] normals;
+
+    // Generated normals for each square of the map
+    private Vector3[] faceNormals;
+
+    // Generated indexes
+    private int[] indexes;
+
+    // Matrix's size = 2^iterationCount + 1
+    // matrixSize² = vertex count
     private int matrixSize;
-
 
 	void Start ()
 	{
+	    MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+	    mesh = meshFilter.mesh;
+
 	    matrixSize = (int)Mathf.Pow(2, iterationCount) + 1;
 	    step = matrixSize - 1;
 
-        heights = new float[matrixSize * matrixSize];
-	    GenerateVertices();
+        vertices = new Vector3[matrixSize * matrixSize];
+	    indexes = new int[matrixSize * matrixSize * 6];
+	    normals = new Vector3[matrixSize * matrixSize];
+	    faceNormals = new Vector3[matrixSize * matrixSize];
 
-	    for (int y = 0; y < matrixSize; y++)
-	    {
-	        for (int x = 0; x < matrixSize; x++)
-	        {
-	            Instantiate(prefab, new Vector3(x, getHeight(x, y), y), Quaternion.identity);
-	        }
-	    }
+	    GenerateVertices();
+	    GenerateIndexes();
+	    GenerateVertexNormals();
 	}
 
     void GenerateVertices()
@@ -101,19 +118,112 @@ public class DiamondSquare : MonoBehaviour {
             }
             step = offset;
         }
+
+        mesh.vertices = vertices;
     }
 
-	void Update () {
-		
-	}
+    void GenerateIndexes()
+    {
+        int count = 0;
+        for (int y = 0; y < matrixSize - 1; y++)
+        {
+            for (int x = 0; x < matrixSize - 1; x++)
+            {
+                indexes[count] = y * matrixSize + x;
+                indexes[count + 1] = (y + 1) * matrixSize + x;
+                indexes[count + 2] = (y + 1) * matrixSize + x + 1;
 
+                indexes[count + 3] = y * matrixSize + x;
+                indexes[count + 4] = (y + 1) * matrixSize + x + 1;
+                indexes[count + 5] = y * matrixSize + x + 1;
+
+                count += 6;
+
+                GenerateFaceNormal(vertices[y * matrixSize + x],
+                                   vertices[(y + 1) * matrixSize + x],
+                                   vertices[(y + 1) * matrixSize + x + 1],
+                                   x, y);
+            }
+        }
+
+        mesh.triangles = indexes;
+    }
+
+    void GenerateFaceNormal(Vector3 p1, Vector3 p2, Vector3 p3, int x, int y)
+    {
+        faceNormals[y * matrixSize + x] = Vector3.Cross(p1 - p2, p3 - p2).normalized;
+    }
+
+    void GenerateVertexNormals()
+    {
+        for (int y = 0; y < matrixSize; y++)
+        {
+            for (int x = 0; x < matrixSize; x++)
+            {
+                int count = 0;
+                Vector3 sum = new Vector3(0.0f, 0.0f, 0.0f);
+                if (isPointInsideMatrix(x - 1, y))
+                {
+                    sum += getFaceNormal(x - 1, y);
+                    count++;
+                }
+
+                if (isPointInsideMatrix(x + 1, y))
+                {
+                    sum += getFaceNormal(x + 1, y);
+                    count++;
+                }
+
+                if (isPointInsideMatrix(x, y - 1))
+                {
+                    sum += getFaceNormal(x, y - 1);
+                    count++;
+                }
+
+                if (isPointInsideMatrix(x, y - 1))
+                {
+                    sum += getFaceNormal(x, y - 1);
+                    count++;
+                }
+
+                Vector3 average = -(sum / count).normalized;
+                
+                normals[y * matrixSize + x] = average;
+
+                if (isPointInsideMatrix(x, y + 1))
+                    normals[(y + 1) * matrixSize + x] = average;
+
+                if (isPointInsideMatrix(x + 1, y + 1))
+                    normals[(y + 1) * matrixSize + x + 1] = average;
+
+                if (isPointInsideMatrix(x, y + 1))
+                    normals[y * matrixSize + x + 1] = average;
+            }
+        }
+
+        mesh.normals = normals;
+    }
+
+    bool isPointInsideMatrix(int x, int y)
+    {
+        return x >= 0 && x < matrixSize && y >= 0 && y < matrixSize;
+    }
+
+    // Returns the height at the given coordinates
     float getHeight(int x, int y)
     {
-        return heights[y * matrixSize + x];
+        return vertices[y * matrixSize + x].y;
     }
 
+    // Returns the normal of the face at the given coordinates
+    Vector3 getFaceNormal(int x, int y)
+    {
+        return faceNormals[y * matrixSize + x];
+    }
+
+    // Set the height of a vertex at a given coordinate
     void setHeight(int x, int y, float value)
     {
-        heights[y * matrixSize + x] = value;
+        vertices[y * matrixSize + x].Set(x, value, y);
     }
 }
