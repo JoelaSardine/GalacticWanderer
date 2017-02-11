802807bdcf85cd5558ba23f59c52fa7939226ceb
Foucauld <foucauld.bouvattier@gmail.com>
2017-02-11 13:08:01 +0100

    Intérieur du vaisseau
    deplacement du joueur dans le vaisseau

---
+++ UnityProject/Assets/Scripts/Navigation/Character/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public Transform Body;
+    public Transform Eye;
+    public float speed = 2.0f;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
+        {
+            move += Eye.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            move -= Eye.forward;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            move += Eye.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
+        {
+            move -= Eye.right;
+        }
+        move.y = 0;
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        Eye.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        Body.position += Vector3.Normalize(move) * speed * Time.deltaTime;
+    }
+}
